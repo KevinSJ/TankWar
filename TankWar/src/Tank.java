d693d3dafe0746891fa89eeec51989af7f4ed167
KevinSJ <kevin4313@163.com>
2017-07-02 12:09:56 -0500

    Complete implmentation of move in 8 direction.

--- TankWar/src/Tank.java
+++ TankWar/src/Tank.java
@@ -11,7 +11,17 @@ import java.awt.event.KeyEvent;
  *
  */
 public class Tank {
-	int x, y;
+	private int x, y;
+	private static final int X_SPEED = 5;
+	private static final int Y_SPEED = 5;
+
+	private boolean bL = false, bU = false, bR = false, bD = false;
+
+	enum Direction {
+		L, LU, U, RU, R, RD, D, LD, STEADY
+	};
+
+	private Direction dir = Direction.STEADY;
 
 	/**
 	 * @param x
@@ -27,24 +37,82 @@ public class Tank {
 		g.setColor(Color.RED);
 		g.fillOval(x, y, 30, 30);
 		g.setColor(c);
+		move();
+	}
+
+	public void move() {
+		switch (dir) {
+		case L:
+			x -= X_SPEED;
+			break;
+		case LU:
+			x -= X_SPEED;
+			y -= Y_SPEED;
+			break;
+		case U:
+			y -= Y_SPEED;
+			break;
+		case RU:
+			x += X_SPEED;
+			y -= Y_SPEED;
+			break;
+		case R:
+			x += X_SPEED;
+			break;
+		case RD:
+			x += X_SPEED;
+			y += Y_SPEED;
+			break;
+		case D:
+			y += Y_SPEED;
+			break;
+		case LD:
+			x -= X_SPEED;
+			y += Y_SPEED;
+			break;
+		case STEADY:
+			break;
+		}
 	}
 
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 		switch (key) {
 		case KeyEvent.VK_LEFT:
-			x -= 5;
+			bL = true;
 			break;
 		case KeyEvent.VK_UP:
-			y -= 5;
+			bU = true;
 			break;
 		case KeyEvent.VK_RIGHT:
-			x += 5;
+			bR = true;
 			break;
 		case KeyEvent.VK_DOWN:
-			y += 5;
+			bD = true;
 			break;
 		}
+		locateDir();
 	}
 
+	public void locateDir() {
+		if (bL && !bU && !bR && !bD)
+			dir = Direction.L;
+		else if (bL && bU && !bR && !bD)
+			dir = Direction.LU;
+		else if (!bL && bU && !bR && !bD)
+			dir = Direction.U;
+		else if (!bL && bU && bR && !bD)
+			dir = Direction.RU;
+		else if (!bL && !bU && bR && !bD)
+			dir = Direction.R;
+		else if (!bL && !bU && bR && bD)
+			dir = Direction.RD;
+		else if (!bL && !bU && !bR && bD)
+			dir = Direction.D;
+		else if (bL && !bU && !bR && bD)
+			dir = Direction.LD;
+		else if (!bL && !bU && !bR && !bD)
+			dir = Direction.STEADY;
+
+	}
 }
